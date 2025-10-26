f15bcf90fa028fcdcc0816f74b5e9dcae77a5422
Edgar <yeai22@live.com>
2025-10-26 17:06:35 -0500

    Load data set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
 import numpy as np
 
+#load data set
+data_set = pd.read_csv("C:/Users/yeai2_6rsknlh/OneDrive/Visual/D600 Task 2/D600 Task 2 Dataset 1 Housing Information.csv")
 
