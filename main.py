0302e637c3f0d24c072d95631d1eb91f841bc89f
Edgar <yeai22@live.com>
2025-10-26 17:35:38 -0500

    Adjustments due to errors

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import pandas as pd
 import numpy as np
 
 #load data set
-data_set = pd.read_csv("C:/Users/yeai2_6rsknlh/OneDrive/Visual/D600 Task 2/D600 Task 2 Dataset 1 Housing Information.csv")
+data_set = pd.read_csv('C:/Users/yeai2_6rsknlh/OneDrive/Visual/D600 Task 2/D600 Task 2 Dataset 1 Housing Information.csv')
 
-#Variables for C2
-variables = ['IsLuxury', 'Price', 'SquareFootage', 'SchoolRating', 'RenovationQuality','Garage','CrimeRate', 'LocalAmenities']
-analysis_data = data_set[variables]
+# Descriptive stats
+# Define variables for logistic regression
+dep_var = "IsLuxury"
+indep_vars = ["Price", "SquareFootage", "SchoolRating", "RenovationQuality", "Garage", "CrimeRate", "LocalAmenities"]
 
-#Descriptive stats
-desc_stats = analysis_data.describe()
-
-#
