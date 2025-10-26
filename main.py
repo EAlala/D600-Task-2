601635ad1f6fb313e1c077fbd599a71707d0f086
Edgar <yeai22@live.com>
2025-10-26 17:46:29 -0500

    Cleaner presentation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 #load data set
-data_set = pd.read_csv('C:/Users/yeai2_6rsknlh/OneDrive/Visual/D600 Task 2/D600 Task 2 Dataset 1 Housing Information.csv')
+data_set = pd.read_csv("C:/Users/yeai2_6rsknlh/OneDrive/Visual/D600 Task 2/D600 Task 2 Dataset 1 Housing Information.csv")
 
 # Descriptive stats
 # Define variables for logistic regression
@@ -13,10 +13,12 @@ indep_vars = ["Price", "SquareFootage", "SchoolRating", "RenovationQuality", "Ga
 analysis_data_set = data_set[[dep_var] + indep_vars]
 
 #Descriptive stats
-desc_stats = analysis_data_set.describe(include=all).transpose()
+desc_stats = analysis_data_set.describe(include="all").transpose()
 
 #Mode and Range for table
 desc_stats["mode"] = analysis_data_set.mode().iloc[0]
 desc_stats["range"] = desc_stats["max"] - desc_stats["min"]
 
-#
+#Cleaner presentation
+final_stats = desc_stats[["count", "mean", "mode", "std", "min", "max", "range"]]
+
