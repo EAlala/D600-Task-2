9284c0ee670931e9a6d4eb7c0a3720ff1a19022e
Edgar <yeai22@live.com>
2025-10-26 17:24:35 -0500

    Descriptive stats

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import numpy as np
 #load data set
 data_set = pd.read_csv("C:/Users/yeai2_6rsknlh/OneDrive/Visual/D600 Task 2/D600 Task 2 Dataset 1 Housing Information.csv")
 
-#Variables from data
-analysis_columns = ['IsLuxury', 'Price', 'SquareFootage', 'SchoolRating', 'RenovationQuality','Garage','CrimeRate', 'LocalAmenities']
+#Variables for C2
+variables = ['IsLuxury', 'Price', 'SquareFootage', 'SchoolRating', 'RenovationQuality','Garage','CrimeRate', 'LocalAmenities']
+analysis_data = data_set[variables]
+
+#Descriptive stats
+desc_stats = analysis_data.describe()
 
 #
